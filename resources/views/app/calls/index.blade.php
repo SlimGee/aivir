fe5d0f31cd8a3fbee4c73a894282871625944b28
Given Ncube <givenyslim12@gmail.com>
2025-04-27 00:49:31 +0200

    fix: paginationn

--- resources/views/app/calls/index.blade.php
+++ resources/views/app/calls/index.blade.php
@@ -190,14 +190,7 @@
                     <!-- Footer -->
                     <div
                         class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
-                        <div>
-                            <p class="text-sm text-gray-600 dark:text-neutral-400">
-                                <span class="font-semibold text-gray-800 dark:text-neutral-200">{{ $calls->count() }}</span>
-                                results
-                            </p>
-                        </div>
-
-                        <div class="space-x-4">
+                        <div class="space-x-4 w-full">
                             {{ $calls->links() }}
                         </div>
                     </div>
--- resources/views/app/users/index.blade.php
+++ resources/views/app/users/index.blade.php
@@ -178,14 +178,7 @@
                     <!-- Footer -->
                     <div
                         class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
-                        <div>
-                            <p class="text-sm text-gray-600 dark:text-neutral-400">
-                                <span class="font-semibold text-gray-800 dark:text-neutral-200">{{ $users->count() }}</span>
-                                results
-                            </p>
-                        </div>
-
-                        <div>
+                        <div class="w-full">
                             {{ $users->links() }}
                         </div>
                     </div>
